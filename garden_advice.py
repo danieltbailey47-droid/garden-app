7d0a151fc7ec0e138428623abf7a6e9671c76d74
Daniel Bailey <danieltbailey47@gmail.com>
2026-03-25 16:54:16 -0400

    Refactor code into functions

--- garden_advice.py
+++ garden_advice.py
@@ -1,25 +1,39 @@
 # Hardcoded values for the season and plant type
-season = "summer"  # TODO: Replace with input() to allow user interaction.
-plant_type = "flower"  # TODO: Replace with input() to allow user interaction.
+season = input("What season would you like advice for? (Summer, Winter, "
+               "Autumn, Spring)\n").lower()
+plant_type = input("What type of plant would you like advice for?"
+                   "(Flower, Vegetable)\n").lower()
 
-# Variable to hold gardening advice
-advice = ""
 
 # Determine advice based on the season
-if season == "summer":
-    advice += "Water your plants regularly and provide some shade.\n"
-elif season == "winter":
-    advice += "Protect your plants from frost with covers.\n"
-else:
-    advice += "No advice for this season.\n"
+def get_season_advice(season):
+    if season == "summer":
+        season_advice = "Water your plants regularly "
+        "and provide some shade.\n"
+    elif season == "winter":
+        season_advice = "Protect your plants from frost with covers.\n"
+    else:
+        season_advice = "No advice for this season.\n"
+    return season_advice
+
 
 # Determine advice based on the plant type
-if plant_type == "flower":
-    advice += "Use fertiliser to encourage blooms."
-elif plant_type == "vegetable":
-    advice += "Keep an eye out for pests!"
-else:
-    advice += "No advice for this type of plant."
+def get_plant_advice(plant_type):
+    if plant_type == "flower":
+        plant_advice = "Use fertiliser to encourage blooms."
+    elif plant_type == "vegetable":
+        plant_advice = "Keep an eye out for pests!"
+    else:
+        plant_advice = "No advice for this type of plant."
+    return plant_advice
+
+
+season_advice = get_season_advice(season)
+
+plant_advice = get_plant_advice(plant_type)
+
+advice = f"\nDuring {season}, it is important to: {season_advice} " \
+         f"When planting {plant_type}s, it is important to: {plant_advice}"
 
 # Print the generated advice
 print(advice)
